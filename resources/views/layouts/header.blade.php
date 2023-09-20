7d83d7d216c8a6094ac728c42229b9f8c72b949a
Fevider <fevider@mail.ru>
2023-09-20 20:07:18 +0400

    Сверстал header

---
+++ resources/views/layouts/header.blade.php
@@ -0,0 +1,40 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+    <link rel="stylesheet" href="{{asset('css/style.css')}}">
+</head>
+<body>
+    <header class="header">
+       <div class="header__leftelem">
+            <div class="header__leftelem-logo">
+                <img src="/images/Logo.png" alt="Logo image">
+            </div>
+            <div class="header__leftelem-search">
+                <input type="text" placeholder="Поиск..." name="search">
+            </div>
+       </div>
+       <div class="header__rightelem">
+            <div class="header__rightelem-text">
+                <a class="rightelem__text" href="#">Создай своё мероприятие</a>
+                
+            </div>
+            <div class="header__rightelem-profile">
+                <img class="rightelem__profile-image">
+                </img>
+                <p class="rightelem__profile-name">NikeName</p>
+            </div>
+            <div class="header__rightelem-entrance">
+                <a class="rightelem__entrance-text" href="#">Войти или зарегестрироваться</a>
+            </div>
+            <div class="header__rightelem-burgermenu ">
+                <span class="rightelem__burgermenu-top"></span>
+                <span class="rightelem__burgermenu-mid"></span>
+                <span class="rightelem__burgermenu-bot"></span>
+            </div>
+       </div>
+    </header>
+</body>
+</html>
